54fcd867c768b9647119b20662b3e54d2f00df06
harmonica8 <monitob875@gmail.com>
2021-11-24 16:32:52 +0200

    test

--- test.php
+++ test.php
@@ -4,7 +4,10 @@
         <?php
             echo "<h2>Hello world!</h2>";
             echo "Some text";
-            echo "Even more some text"
+            echo "Even more some text";
+            echo "Thank you for the even more some text";
+            $txt = "Heading";
+            echo "<h2>".$txt."</h2>";
         ?>
     </body>
 </html>
