a2eaf0cab2d73f4c3242a919f5ccf4b0c2ee6769
harmonica8 <monitob875@gmail.com>
2021-11-24 16:46:36 +0200

    test

--- test.php
+++ test.php
@@ -8,6 +8,25 @@
             echo "Thank you for the even more some text";
             $txt = "Heading";
             echo "<h2>".$txt."</h2>";
+            echo "It's time for some OOP";
+            class PhpOOP {
+                public $name;
+                public $age;
+
+                function set_name($name) {
+                    $this -> name = $name;
+                }
+                function get_name() {
+                    return $this -> name;
+                }
+
+                function set_age($age) {
+                    $this -> age = $age;
+                }
+                function get_age() {
+                    $this -> age;
+                }
+            }
         ?>
     </body>
 </html>
