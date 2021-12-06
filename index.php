872f7f723898e6499bd3fbb3d33879fc751be87a
harmonica8 <monitob875@gmail.com>
2021-12-06 09:49:44 +0200

    data

--- index.php
+++ index.php
@@ -5,10 +5,9 @@ $database;
 $connection = new DBConnection();
 $database = $connection->db_connect();
 
-//$database->query("INSERT INTO banks_info (name, bic, iban) VALUES ('UNICREDIT', '47214872472', 'bg436492381f61364296f21')");
-//$database->query("INSERT INTO clients_providers (name, address, country, city, number, vat_number, acc_person, type, bank_ID) VALUES ('Monika EOOD', 'Skopie 45', 'Bulgaria', 'Plovdiv', '14135263452', 'BG14135263452', 'Kiril', 1, 6)");
-//$database->query("SELECT * FROM clients_providers");
-//echo "Affected rows: " . $database -> affected_rows;
+$database->query("INSERT INTO banks_info (name, bic, iban) VALUES ('UNICREDIT', '47214872472', 'bg436492381f61364296f21')");
+$database->query("INSERT INTO clients_providers (name, address, country, city, number, vat_number, acc_person, type, bank_ID) VALUES ('Monika EOOD', 'Skopie 45', 'Bulgaria', 'Plovdiv', '14135263452', 'BG14135263452', 'Kiril', 1, 6)");
+
 $sql = "SELECT ID, name, iban FROM banks_info";
 $result = $database->query($sql);
 
